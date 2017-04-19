696bb12b6af35f37baaefa6c8590da61aee4a2fe
Juan D. Acosta <juandacorias@gmail.com>
2017-04-19 09:07:43 -0500

    Started State Design

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchIP } from '../actions';
+// Material UI
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import { MuiThemeProvider, getMuiTheme } from 'material-ui/styles';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// My Components
 import RouterDemo from '../components/RouterDemo';
 
 class App extends Component {
+  componentWillMount() {
+    // Inside the Lifecycle hook to fix the Hot Reloading Issue
+    injectTapEventPlugin();
+  }
+
   componentDidMount() {
     this.props.fetchIP();
   }
@@ -11,33 +21,35 @@ class App extends Component {
   render() {
     const { ip, isFetching } = this.props;
     return (
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <div>
-          <h2>Welcome to my MERN Boilerplate</h2>
+      <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <div>
+            <h2>Welcome to my MERN Boilerplate</h2>
+          </div>
+          <p>
+            This is a simple working component{' '}
+          </p>
+          {isFetching
+            ? <p style={{ color: 'red' }}>Loading</p>
+            : <p style={{ color: 'blue' }}>Your IP address is: {ip}</p>}
+          <RouterDemo />
+          <p style={{ textAlign: 'center' }}>
+            For Syncing React Router with Redux Checkout <br />
+            <a
+              href="https://github.com/ReactTraining/react-router/tree/master/packages/react-router-redux"
+              target="_blank"
+            >
+              react-router-redux
+            </a>
+          </p>
         </div>
-        <p>
-          This is a simple working component{' '}
-        </p>
-        {isFetching
-          ? <p style={{ color: 'red' }}>Loading</p>
-          : <p style={{ color: 'green' }}>Your IP address is: {ip}</p>}
-        <RouterDemo />
-        <p style={{textAlign: 'center'}}>
-          For Syncing React Router with Redux Checkout <br/>
-          <a
-            href="https://github.com/ReactTraining/react-router/tree/master/packages/react-router-redux"
-            target="_blank"
-          >
-            react-router-redux
-          </a>
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
