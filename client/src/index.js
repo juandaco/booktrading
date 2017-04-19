9ea57429f271ed2c6c314e254b9d9e113ac39d7d
Juan D. Acosta <juandacorias@gmail.com>
2017-04-18 19:21:15 -0500

    Repaired Redux Store Hot Reloading Issue

--- client/src/index.js
+++ client/src/index.js
@@ -1,34 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import reducer from './reducers';
+import store from './store';
 import Root from './containers/Root';
 import './index.css';
-
-const middleware = [thunk];
-
-/*
-  Add middleware based on enviroment if desired
-*/
-// if (process.env.NODE_ENV !== 'production') {
-//   middleware.push(createLogger());
-// }
-
-/*
-  For Redux Dev Tools in Chrome
-*/
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-/*
-  Explicit Hot Reloading Reducer (see https://github.com/reactjs/react-redux/releases/tag/v2.0.0)
-*/
-const store = createStore(
-  reducer,
-  // For Redux Dev Tools and Middlware
-  composeEnhancers(applyMiddleware(...middleware)),
-);
 
 render(
   <AppContainer>
@@ -41,18 +16,13 @@ render(
   Enabling Component Hot Reloading
 */
 if (module.hot) {
-  module.hot.accept('./reducers', () => {
-    const nextReducer = require('./reducers').default;
-    store.replaceReducer(nextReducer);
-
-    module.hot.accept('./containers/Root', () => {
-      const NextRoot = require('./containers/Root').default;
-      render(
-        <AppContainer>
-          <NextRoot />
-        </AppContainer>,
-        document.getElementById('root'),
-      );
-    });
+  module.hot.accept('./containers/Root', () => {
+    const NextRoot = require('./containers/Root').default;
+    render(
+      <AppContainer>
+        <NextRoot store={store}/>
+      </AppContainer>,
+      document.getElementById('root'),
+    );
   });
 }
