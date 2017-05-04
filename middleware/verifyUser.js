561b060187e2fe3a5fe955a1a11f8919ed591456
Juan D. Acosta <juandacorias@gmail.com>
2017-05-04 08:38:44 -0500

    Heroku Deployment

--- middleware/verifyUser.js
+++ middleware/verifyUser.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res, next) {
   if (!req.isAuthenticated()) {
-    return res.status(401).json({
+    return res.json({
       errorMsg: 'You need to login first',
     });
   }
